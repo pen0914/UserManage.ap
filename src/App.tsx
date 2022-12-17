511001dd0930c7d15c5a3f5be0ab9a6a05fb4a0f
pen0914 <115807679+pen0914@users.noreply.github.com>
2022-12-17 18:04:22 +0900

    Routeコンポーネントの作成 Home/setting/user_management/404

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-import "./styles.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
+  );
 }
